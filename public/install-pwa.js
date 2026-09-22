bef574ccf31d9a51be6a3fa9a680d2b3a7a1f4c4
issaapplication-blip <issaapplication@gmail.com>
2026-09-22 22:28:00 +0300

    stabilize install button label and service worker version

--- public/install-pwa.js
+++ public/install-pwa.js
@@ -141,7 +141,7 @@
   const start=()=>{
     cleanDuplicates();ensurePublicMembershipUi();installApplicationPersistence();ensureInstallButton();
     if(navigator.serviceWorker){
-      navigator.serviceWorker.register('/sw.js?v=48',{updateViaCache:'none'}).catch(()=>{});navigator.serviceWorker.ready.then(()=>{
+      navigator.serviceWorker.register('/sw.js?v=49',{updateViaCache:'none'}).catch(()=>{});navigator.serviceWorker.ready.then(()=>{
         if(!navigator.serviceWorker.controller&&!sessionStorage.getItem('RAFIQ_SW_CONTROL_RELOAD_20260922_V2')){
           sessionStorage.setItem('RAFIQ_SW_CONTROL_RELOAD_20260922_V2','1');
           location.reload();
