5119d5dbcf7d1b54986b416ee901dc104be60b3e
issaapplication-blip <issaapplication@gmail.com>
2026-09-23 22:17:53 +0300

    fix: refresh PWA installer after logo and service worker repair

--- public/install-pwa.js
+++ public/install-pwa.js
@@ -141,9 +141,9 @@
   const start=()=>{
     cleanDuplicates();ensurePublicMembershipUi();installApplicationPersistence();ensureInstallButton();
     if(navigator.serviceWorker){
-      navigator.serviceWorker.register('/sw.js?v=53',{updateViaCache:'none'}).catch(()=>{});navigator.serviceWorker.ready.then(()=>{
-        if(!navigator.serviceWorker.controller&&!sessionStorage.getItem('RAFIQ_SW_CONTROL_RELOAD_20260923_V1')){
-          sessionStorage.setItem('RAFIQ_SW_CONTROL_RELOAD_20260923_V1','1');
+      navigator.serviceWorker.register('/sw.js?v=54',{updateViaCache:'none'}).catch(()=>{});navigator.serviceWorker.ready.then(()=>{
+        if(!navigator.serviceWorker.controller&&!sessionStorage.getItem('RAFIQ_SW_CONTROL_RELOAD_20260923_V2')){
+          sessionStorage.setItem('RAFIQ_SW_CONTROL_RELOAD_20260923_V2','1');
           location.reload();
         }
       }).catch(()=>{});
